bfac717cb0868ff97452bb49f11a047a7dc30875
Panneer Selvam Shanmugam <panneerss.altimetrik@gmail.com>
2024-02-07 19:44:42 +0530

    anagram change

--- src/main/java/org/example/Anagram.java
+++ src/main/java/org/example/Anagram.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Anagram {
     public static void main(String[] args) {
@@ -13,5 +15,15 @@ public class Anagram {
         Arrays.sort(string1Array);
         Arrays.sort(string2Array);
         return new String(string1Array).equals(new String(string2Array));
+    }
+
+    private static boolean isAnagram2(String a,String b){
+        return sortedCaseInsensitiveString(a).equals(sortedCaseInsensitiveString(b));
+    }
+
+    private static String sortedCaseInsensitiveString(String str){
+        var collect = str.chars().mapToObj(c -> (char) c).map(Objects::toString).map(String::toLowerCase).collect(Collectors.joining());
+        System.out.println(collect);
+        return collect;
     }
 }
